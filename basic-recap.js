58d590c81d8e69156847c624c4af33b1c5f90bd7
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-21 17:12:04 +0600

    basic js recap

---
+++ basic-recap.js
@@ -0,0 +1,84 @@
+// * variable 
+var deposit = 400;
+
+// * condition 
+// 500 cheye boro kina / beshi kina
+if(deposit > 500 ){
+//  500 cheye choto kina / kom kin 
+}else if( deposit < 200){
+
+    // 500 er soman kin 
+}else if(deposit == 500){
+
+    // soman na kina 
+}else if(deposit != 500){
+
+    // 500 er boro ba soman kina 
+}else if(deposit >= 500){
+
+    // choto ba soman kina
+}else if(deposit<= 500){
+
+    // konotai tai an hole 
+}else{
+
+}
+// * Array 
+
+var array = [45, 44, 222, 887, 88, 90, 69,]
+
+// array te koita element ase seta jante 
+var numberCount = array.length;
+
+// array te new element add korte 
+array.push(33);
+// array teke Element remove korte 
+array.pop()
+// array kono index er value change korte 
+array[2]= 555;
+
+// array te kono element ase kin na ta jante 
+// if( array.indexOf(222) != -1){
+//     console.log("222 nai")
+// }else{
+//     console.log('222 ase')
+// }
+
+// if(array.includes(23)){
+//     console.log(true)
+// }else{
+//     console.log(false);
+// }
+
+
+// console.log(array)
+// console.log(numberCount)
+
+// * loop 
+// while loop 
+// for(i < 0 ){
+    
+// }
+// for loop 
+
+// for /of loop 
+
+
+//  function 
+
+function fullName(first,second){
+    var num = first + second;
+    return num
+}
+var name = fullName ('sabuj', +""+ 'mahonto')
+console.log(name)
+
+//* Object
+
+var produc ={
+    name:'laptop',
+    color:'silver',
+    price:100000,
+}
+console.log(produc.price)
+
