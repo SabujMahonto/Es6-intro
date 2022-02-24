4b9e008413503d27dbd2e9d2b01ffc80f3c105cf
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-24 20:21:54 +0600

    template strings concept done

---
+++ template-string.js
@@ -0,0 +1,30 @@
+
+// old version
+// let multiline = 'this is first line \n' + 'this is second line\n'+ 
+//                 'this is thard line code'
+
+
+// update version 
+
+// let multiline = `this is first line
+//                 this is second line 
+//                this is third line `
+
+// console.log(multiline)
+ 
+// old away
+
+let firstName = 'mamun'
+let lastName = 'chodhori'
+// let fullName = firstName +" "+ lastName;
+// New away
+let fullName = `${firstName} ${lastName}`
+
+// console.log(fullName);
+
+let nameList = ['mamun', 'jaman', 'kalam', 'salam']
+
+let newlist = `my all friends are ${nameList.length} person`
+console.log(newlist);
+
+
