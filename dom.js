53a9e14e9e38e50260457d6ba6bb6cc522418d84
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-24 18:55:15 +0600

    add friend added

---
+++ dom.js
@@ -0,0 +1,38 @@
+// change broder color 
+      document.getElementById('add-border').addEventListener('click', function(){
+        
+           let changeBorder = document.getElementById('friends-container');
+           changeBorder.style.border = '3px solid red';
+ 
+     })
+
+     
+
+// change backgroundcolor 
+  function addBackgroundColor(){
+     const friends =  document.getElementsByClassName('friend');
+            for(const friend of friends){
+                  friend.style.backgroundColor = 'green';
+            }
+
+     }
+
+//      add friend 
+
+document.getElementById('add-friend').addEventListener('click', function(){
+    const container =  document.getElementById('friends-container');
+    const friendDiv = document.createElement('div');
+            friendDiv.classList.add('friend');
+            friendDiv.innerHTML =`
+            <h3 class="friend-name">new friend</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea veritatis odit obcaecati quisquam laboriosam earum ut eaque molestiae dolorem officiis.</p>
+            `;
+
+            container.appendChild(friendDiv)
+    
+})
+
+
+     
+
+  
